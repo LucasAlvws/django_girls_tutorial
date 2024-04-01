bb0d03253c182f51e3488f6efe73c7a5fa84b673
Lucas Alves <sociallucasalves@gmail.com>
2024-04-01 13:30:35 -0300

    feat: post template and queryset

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
+
+
+def post_list(request):
+    post = Post.objects.all().order_by("published_at")
+    return render(request, "blog/post_list.html", {"post": post})
